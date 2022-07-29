6999f6735950e6586ea53eee5438793bef8ee779
Isaac-websparks <isaac.chong@websparks.sg>
2022-07-29 20:47:47 +0800

    implementing rest api functionality

--- src/App.js
+++ src/App.js
@@ -2,17 +2,27 @@ import { useEffect, useState } from "react";
 import AddNewPerson from "./components/Addnewperson";
 import Filter from "./components/Filter";
 import List from "./components/List";
+import axios from "axios";
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { id: 1, name: "Arto", number: 43 }, 
-    { id: 2, name: "Bob", number: 32 }, 
-    { id: 3, name: "Sam", number: 98 }, 
-    { id: 4, name: "Gamgee", number: 13 }
-  ]);
-  
+  // [
+  //   { id: 1, name: "Arto", number: 43 }, 
+  //   { id: 2, name: "Bob", number: 32 }, 
+  //   { id: 3, name: "Sam", number: 98 }, 
+  //   { id: 4, name: "Gamgee", number: 13 }
+  // ]
+  const [persons, setPersons] = useState([]);
   const [filterName, setFilterName] = useState("")
   const [filteredList, setFilteredList] = useState([])
+
+  const getPersons = () => {
+    axios.get('http://localhost:3001/persons')
+         .then(response => {
+          setPersons(response.data)
+         })
+  }
+
+  useEffect(getPersons, [])
   
   useEffect(() => {
     setFilteredList(persons.filter((person) => person.name.toUpperCase() === filterName.toUpperCase()))
